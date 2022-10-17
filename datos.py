83ad5bdb34122def5d6406446b6a7311516b77a4
castrojavier96 <javier.castro.g96@gmail.com>
2022-10-17 22:51:46 +0200

    datos

--- datos.py
+++ datos.py
@@ -1,2 +1,19 @@
-print('hola')
-print('hola2')
+import pandas as pd
+import glob
+import os
+import matplotlib.pyplot as plt
+
+path = r'C:\Users\javie\Downloads\Archives\ETFs' # use your path
+all_files = glob.glob(os.path.join(path, "*.txt"))
+
+li = []
+
+for filename in all_files:
+    df = pd.read_csv(filename, index_col='Date', header=0)
+    df_close = df.loc['2010-01-01':'2015-01-01',['Close']]
+    li.append(df_close)
+
+frame = pd.concat(li, axis=1, ignore_index=True)
+datos = frame.iloc[:884].dropna(axis=1)
+
+plt.plot(datos)
